bd264aee81f24cd27002d7daba34a55836fe1493
Tanner Donovan <ttdonovan@gmail.com>
2013-03-19 00:54:30 -0700

    explore using router active, deactivate and events for transactions

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ App = Ember.Application.create();
 
 // Routers
 App.Router.map(function() {
-  this.resource('contacts', function() {
+  this.resource('contacts', { path: '/contacts'} , function() {
     // this.resource('contact', { path: ':contact_id' }, function() {
     //   this.route('edit');
     // });
@@ -33,8 +33,8 @@ App.ContactIndexRoute = Ember.Route.extend({
   },
 
   setupController: function(controller, model) {
-    var editController = this.controllerFor('contactEdit');
-    editController.stopEditing();
+    // var editController = this.controllerFor('contactEdit');
+    // editController.stopEditing();
     controller.set('content', model);
   }
 });
@@ -46,7 +46,30 @@ App.ContactEditRoute = Ember.Route.extend({
 
   setupController: function(controller, model) {
     controller.set('content', model);
-    controller.startEditing();
+    // controller.startEditing();
+  },
+
+  activate: function() {
+    // var contact = this.get('currentModel'),
+    //   transaction = contact.get('transaction');
+    // transaction.add(contact);
+  },
+
+  deactivate: function() {
+    // this.modelFor('contactEdit').get('transaction').rollback();
+  },
+
+  events: {
+    saveContact: function() {
+      // debugger;
+      // contact.get('transaction').commit();
+      this.transitionTo('contact', this.get('currentModel'));
+    },
+
+    cancelContact: function() {
+      // Uncaught RangeError: Maximum call stack size exceeded
+      this.transitionTo('contact', this.get('currentModel'));
+    }
   }
 });
 
@@ -55,39 +78,41 @@ App.ContactsController = Ember.ArrayController.extend({
   sortProperties: ['lastName']
 });
 
-App.ContactEditController = Ember.ObjectController.extend({
-  startEditing: function() {
-    // var controller = this.get('content');
-    // var transaction = controller.get('store').transaction();
-    // transaction.add(controller.get('model'));
-    // this.transaction = transaction;
-  },
-
-  stopEditing: function() {
-    // var transaction = this.transaction;
-    // if (transaction) {
-    //   transaction.rollback();
-    //   this.transaction = undefined;
-    // }
-  },
-
-  save: function() {
-    // var transaction = this.transaction;
-    // if (transaction) {
-    //   transaction.commit();
-    //   this.transaction = undefined;
-    //   this.transitionToRoute('contact', this.get('content'));
-    // }
-  },
-
-  cancel: function() {
-    // var transaction = this.transaction;
-    // if (transaction) {
-    //   this.stopEditing();
-    //   this.transitionToRoute('contact', this.get('content'));
-    // }
-  }
-});
+// App.ContactEditController = Ember.ObjectController.extend({
+//   needs: ['contact']
+//
+//   startEditing: function() {
+//     // var controller = this.get('content');
+//     // var transaction = controller.get('store').transaction();
+//     // transaction.add(controller.get('model'));
+//     // this.transaction = transaction;
+//   },
+//
+//   stopEditing: function() {
+//     // var transaction = this.transaction;
+//     // if (transaction) {
+//     //   transaction.rollback();
+//     //   this.transaction = undefined;
+//     // }
+//   },
+//
+//   saveContact: function() {
+//     // var transaction = this.transaction;
+//     // if (transaction) {
+//     //   transaction.commit();
+//     //   this.transaction = undefined;
+//     //   this.transitionToRoute('contact', this.get('content'));
+//     // }
+//   },
+//
+//   cancelContact: function() {
+//     // var transaction = this.transaction;
+//     // if (transaction) {
+//     //   this.stopEditing();
+//     //   this.transitionToRoute('contact', this.get('content'));
+//     // }
+//   }
+// });
 
 // Views
 App.ContactFormView = Ember.View.extend({
@@ -99,7 +124,7 @@ App.ContactFormView = Ember.View.extend({
   },
 
   submit: function() {
-    this.get('controller').send('save');
+    this.get('controller').send('saveContact');
     return false;
   }
 });
